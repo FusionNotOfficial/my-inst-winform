c9aa6d9f025651f822ba0d7c6b810a6b110357ca
Mikita Shabanau <bags4circles@gmail.com>
2024-01-08 22:25:56 +0900

    db backup indahaus

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -57,64 +57,17 @@ CREATE TABLE Followers
 	f_followingId INT FOREIGN KEY REFERENCES Users(u_id)
 )
 
-SELECT * FROM Followers
-DELETE FROM Followers
-
-SELECT COUNT(f_followingId) FROM Followers WHERE f_followingId = 1
-
-INSERT INTO Rooms(sender, reciever) VALUES(1,2)
-INSERT INTO Rooms(sender, reciever) VALUES(1,3)
-INSERT INTO Rooms(sender, reciever) VALUES(2,3)
-INSERT INTO Rooms(sender, reciever) VALUES(1,5)
-
-INSERT INTO Messages(text_content, send_date, room, sender, checked) VALUES('Hello', GETTIME(), 1, 1, 0)
-
-SELECT * FROM Likes WHERE l_postId = 1 AND l_userId = 3
-
-
-SELECT * FROM Rooms WHERE sender = 2 OR reciever = 2
-
-SELECT TOP 1 text_content FROM Messages WHERE room = 3 ORDER BY m_id DESC
-SELECT COUNT(m_id) FROM Messages WHERE room = 1 AND checked = 0
-
-SELECT COUNT(f_id) FROM Followers WHERE f_userId = 1 AND f_followingId = 19
-SELECT * FROM Followers
-DELETE FROM Followers
-ALTER TABLE Users 
-ADD u_lastSeen datetime
-
-ALTER TABLE Users
-DROP COLUMN u_lastSeen;
-
-drop table Comment
-drop table Post
-drop table Followers
-
-delete from messages
-
-delete from users where u_id = 5
-
-INSERT INTO Post(p_picture, p_date, p_content, p_likes, p_user) VALUES(null, null, 'hello', 0, 1)
-
-DELETE FROM Post
-
-SELECT p_picture, p_date FROM Post WHERE p_user = 1
-DELETE FROM POST WHERE p_id > 9
 
 --DESKTOP-T96O8TL\SQLEXPRESS--
-SELECT p_picture, p_date, p_content, p_likes, u_username, u_picture FROM Post INNER JOIN Users ON Post.p_user = Users.u_id WHERE p_id = 7
-SELECT p_id, p_picture, p_date, p_content, p_likes, u_username, u_picture FROM Post INNER JOIN Users ON Post.p_user = Users.u_id
-SELECT p_id FROM Post
-SELECT COUNT(f_id) FROM Followers WHERE f_followingId = 1
+--SELECT p_picture, p_date, p_content, p_likes, u_username, u_picture FROM Post INNER JOIN Users ON Post.p_user = Users.u_id WHERE p_id = 7
+--SELECT p_id, p_picture, p_date, p_content, p_likes, u_username, u_picture FROM Post INNER JOIN Users ON Post.p_user = Users.u_id
+--SELECT p_id FROM Post
+--SELECT COUNT(f_id) FROM Followers WHERE f_followingId = 1
 
-INSERT INTO Likes(l_userId, l_postId) VALUES(1, 1)
-SELECT * FROM Post WHERE p_id = 18
-DELETE FROM Likes
+--SELECT u_id, u_username, u_picture FROM Followers INNER JOIN Users ON Followers.f_userId = Users.u_id WHERE f_followingId = 1
+--SELECT * FROM Followers
 
-SELECT u_id, u_username, u_picture FROM Followers INNER JOIN Users ON Followers.f_userId = Users.u_id WHERE f_followingId = 1
-SELECT * FROM Followers
+--UPDATE Post SET p_likes = 3 WHERE p_id = 18
+--SELECT DISTINCT u_id, u_username FROM Followers INNER JOIN Users ON Followers.f_followingId = Users.u_id WHERE u_username LIKE 'c%'
 
-UPDATE Post SET p_likes = 3 WHERE p_id = 18
-SELECT DISTINCT u_id, u_username FROM Followers INNER JOIN Users ON Followers.f_followingId = Users.u_id WHERE u_username LIKE 'c%'
-
-SELECT u_id, u_username FROM Rooms INNER JOIN Users ON Rooms.reciever = Users.u_id WHERE u_username LIKE 'c%' AND (sender = 1 OR reciever = 1)
+--SELECT u_id, u_username FROM Rooms INNER JOIN Users ON Rooms.reciever = Users.u_id WHERE u_username LIKE 'c%' AND (sender = 1 OR reciever = 1)
